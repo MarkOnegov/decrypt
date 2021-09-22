a35c9eba0eaf9f331f3113007cb6cf0b1253c7a4
Класс <class@vyatsu.ru>
2021-09-22 09:03:56 +0300

    add symbole rate

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const inputTextArea = document.getElementById("input")
 const outputTextArea = document.getElementById("output")
 const keyInput = document.getElementById("key-input")
+const chars = document.getElementById("chars")
 
 const availableCharters = 'йцукенгшщзхъэждлорпавыфячсмитьбю_'.split('').sort((a, b) => a.localeCompare(b))
 const keyMap = {}
@@ -16,17 +17,26 @@ const generateKeyInput = (ch) => {
     <input type="text" class="key-input__char__output" maxlength="1" data-char="${ch}">
 </span>
     `)
-    keyInput.querySelector(`input[data-char=${ch}]`)?.addEventListener('input', ({ target }) => { keyMap[ch] = target.value; outputTextArea.value = decrypt(inputTextArea.value) })
+    keyInput.querySelector(`input[data-char=${ch}]`)?.addEventListener('input', ({ target }) => { keyMap[ch] = target.value; rewrite() })
 }
 availableCharters.forEach(generateKeyInput)
+
+const printChar = (ch,v)=>
+    chars.insertAdjacentHTML('beforeend',`<span class="char-value${keyMap[ch]?' fill':''}">${ch}: ${v.toFixed(3)}</span>`)
+
+const printChars = (chs) => {
+    chars.innerHTML = "";
+    Object.entries(chs).sort((a,b)=>b[1]-a[1]).forEach(e=>printChar(e[0],e[1]))
+}
 
 const decrypt = (text) =>
     text.split("").map(ch => keyMap[ch]?.toUpperCase() || ch).join("")
 
-inputTextArea.addEventListener('input', () => {
+const rewrite = ()=>{
     const chs = {}
     inputTextArea.value.split('').forEach(ch => chs[ch] && chs[ch]++ || (chs[ch] = 1))
     Object.keys(chs).forEach((ch) => chs[ch] /= inputTextArea.value.length)
-    console.log(chs);
+    printChars(chs);
     outputTextArea.value = decrypt(inputTextArea.value)
-})
+}
+inputTextArea.addEventListener('input', () => rewrite())
